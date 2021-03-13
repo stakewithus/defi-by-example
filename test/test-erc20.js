0cf8d29d6f5b752ffe6564c4e280224d28380423
taz <tsk.nakamura@gmail.com>
2021-03-13 12:27:00 +0900

    erc20 transfer example

--- test/test-erc20.js
+++ test/test-erc20.js
@@ -16,4 +16,9 @@ contract("IERC20", (accounts) => {
     const bal = await token.balanceOf(WHALE);
     console.log(`bal: ${bal}`);
   });
+
+  it("should transfer", async () => {
+    const bal = await token.balanceOf(WHALE);
+    await token.transfer(accounts[0], bal, { from: WHALE });
+  });
 });
