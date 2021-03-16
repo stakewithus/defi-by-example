2ad5c1f6f520b92f5dd0b42cb7a0e902d0bbf0ac
taz <tsk.nakamura@gmail.com>
2021-03-16 17:22:43 +0900

    todo

--- contracts/TestUniswap.sol
+++ contracts/TestUniswap.sol
@@ -5,10 +5,8 @@ import "./interfaces/IERC20.sol";
 import "./interfaces/Uniswap.sol";
 
 contract TestUniswap {
-  // TODO: what is router?
   address private constant UNISWAP_V2_ROUTER =
     0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-  // TODO: why need weth?
   address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
   function swap(
