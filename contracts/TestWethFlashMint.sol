de984246e5d0ffdd0368e7d0791f249d89826b45
taz <tsk.nakamura@gmail.com>
2021-06-21 06:56:23 +0900

    weth 10 - flash mint

--- contracts/TestWethFlashMint.sol
+++ contracts/TestWethFlashMint.sol
@@ -5,27 +5,44 @@ import "./interfaces/WETH10.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TestWethFlashMint {
+  // WETH 10
   address private WETH = 0xf4BB2e28688e89fCcE3c0580D37d36A7672E8A9F;
   bytes32 public immutable CALLBACK_SUCCESS =
     keccak256("ERC3156FlashBorrower.onFlashLoan");
 
+  address public sender;
+  address public token;
+
   event Log(string name, uint val);
 
   function flash() external {
-    uint amount = 10**18;
+    uint total = IERC20(WETH).totalSupply();
+    // borrow more than available
+    uint amount = total + 1;
+
+    emit Log("total supply", total);
+
     IERC20(WETH).approve(WETH, amount);
-    WETH10(WETH).flashLoan(address(this), WETH, amount, "");
+
+    bytes memory data = "";
+    WETH10(WETH).flashLoan(address(this), WETH, amount, data);
   }
 
   // called by WETH10
   function onFlashLoan(
-    address sender,
-    address token,
+    address _sender,
+    address _token,
     uint amount,
     uint fee,
     bytes calldata data
   ) external returns (bytes32) {
     uint bal = IERC20(WETH).balanceOf(address(this));
+
+    sender = _sender;
+    token = _token;
+
+    emit Log("amount", amount);
+    emit Log("fee", fee);
     emit Log("balance", bal);
 
     return CALLBACK_SUCCESS;
--- test/test-weth-flash-mint.js
+++ test/test-weth-flash-mint.js
@@ -1,22 +1,26 @@
-const BN = require("bn.js");
-const { WETH_10 } = require("./config");
+const BN = require("bn.js")
+const { WETH_10 } = require("./config")
 
-const IERC20 = artifacts.require("IERC20");
-const TestWethFlashMint = artifacts.require("TestWethFlashMint");
+const IERC20 = artifacts.require("IERC20")
+const TestWethFlashMint = artifacts.require("TestWethFlashMint")
 
 contract("TestWethFlashMint", (accounts) => {
-  let testWethFlashMint;
-  let weth;
+  let testWethFlashMint
+  let weth
   beforeEach(async () => {
-    weth = await IERC20.at(WETH_10);
-    testWethFlashMint = await TestWethFlashMint.new();
-  });
+    weth = await IERC20.at(WETH_10)
+    testWethFlashMint = await TestWethFlashMint.new()
+  })
 
   it("flash", async () => {
-    const tx = await testWethFlashMint.flash();
+    const tx = await testWethFlashMint.flash()
+
+    console.log(`contract: ${await testWethFlashMint.address}`)
+    console.log(`sender: ${await testWethFlashMint.sender()}`)
+    console.log(`token: ${await testWethFlashMint.token()}`)
 
     for (const log of tx.logs) {
-      console.log(`${log.args.name} ${log.args.val}`);
+      console.log(`${log.args.name} ${log.args.val}`)
     }
-  });
-});
+  })
+})
